9404ac1a2da040f95c1d008aedd5221cc507d364
gleydson-borges <gleydsonrodrigues@ymail.com>
2016-07-16 01:03:51 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="pt">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Kodando</title>
+</head>
+<link rel="stylesheet" href="css/normalize.css" />
+<link rel="stylesheet" href="css/main.css" />
+<link rel="stylesheet" href="css/magic.min.css" />
+<link rel="stylesheet" href="css/bootstrap.css" />
+<body>
+<div class="container">
+    <div class="col-md-12 content">
+        <div class="text-center">
+            <div class="box">
+                <div class="box-content">
+                    <div class="box-wrapper">
+                        <h1>Em Breve</h1>
+                        <p>Desenvolvimento de sistemas. <br> Melhorias, agilidade e produtividade para sua empresa.</p>
+                    </div>
+                </div>
+                <div class="box-overlay magictime rotate" data-hover="puffOut" data-return="puffIn">
+                    <img src="img/logo_v1.png" class="img img-responsive" alt="Logo Kodando Sistemas   ">
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<script src="js/jquery.1.10.2.min.js"></script>
+<script src="js/main.js"></script>
+</html>
